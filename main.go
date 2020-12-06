b7a1ebc3f0d00a9c0b4450dad7761a322de729e1
Paul Pardee <paul.pardee@gmail.com>
2020-12-06 16:17:45 -0700

    Moved to struct for puzzle data

--- main.go
+++ main.go
@@ -19,15 +19,14 @@ const nine = 1 << 8
 // We're dealing with 9 bits, 255 + 256 = 511
 const allOnes = 511
 
-var puzzle [9][9]int
+var puzzle solver.SudokuPuzzle
 
 // Creating a guesses matrix for complex solves
 var guesses [9][9]int
 
-// We need to know how many cells are left to solve so we know when we are done/stuck
-var unknownCount int
-
 func main() {
+	// We need to know how many cells are left to solve so we know when we are done/stuck
+	var unknownCount int
 
 	passCounter := 1
 	var ssClearCount int = 0
@@ -37,14 +36,14 @@ func main() {
 			fmt.Printf("Pass %v\n", passCounter)
 			fmt.Println("----------")
 
-			ssClearCount, unknownCount = solver.SimpleSolve(&puzzle, &guesses)
+			ssClearCount = solver.SimpleSolve(&puzzle)
 
 			fmt.Printf("Pass %v cleared %v cells!", passCounter, ssClearCount)
 
 			solver.PrintPuzzle(&puzzle)
 
 			if ssClearCount == 0 {
-				if unknownCount > 0 {
+				if puzzle.UnknownCount > 0 {
 					fmt.Printf("simpleSolve failed with %v cells unsolved.\n", unknownCount)
 					break
 				} else {
@@ -57,12 +56,12 @@ func main() {
 
 		for {
 			fmt.Println("Running complexSolve")
-			csClearCount, unknownCount = solver.ComplexSolve(&puzzle, &guesses)
+			csClearCount = solver.ComplexSolve(&puzzle)
 			solver.PrintPuzzle(&puzzle)
 			if csClearCount == 0 {
 				if unknownCount > 0 {
 					fmt.Printf("complexSolve failed with %v cells unsolved. Running SimpleSolve again\n", unknownCount)
-					ssClearCount, unknownCount = solver.SimpleSolve(&puzzle, &guesses)
+					ssClearCount = solver.SimpleSolve(&puzzle)
 					break
 				} else {
 					fmt.Println("Solve complete!")
@@ -85,29 +84,29 @@ func main() {
 func init() {
 
 	// This is the initial state of the puzzle we're trying to solve
-	// easy := [9][9]int{
-	// 	{0, 8, 0, 7, 0, 9, 0, 0, 2},
-	// 	{0, 3, 4, 0, 1, 0, 0, 9, 0},
-	// 	{0, 0, 0, 3, 0, 8, 0, 0, 0},
-	// 	{0, 0, 6, 4, 3, 0, 8, 0, 1},
-	// 	{0, 0, 1, 2, 7, 6, 0, 4, 0},
-	// 	{0, 0, 3, 0, 0, 1, 2, 5, 6},
-	// 	{0, 0, 0, 0, 9, 0, 0, 2, 7},
-	// 	{3, 4, 0, 8, 6, 7, 9, 0, 0},
-	// 	{0, 9, 0, 5, 0, 4, 0, 0, 3},
-	// }
-
-	medium := [9][9]int{
-		{1, 3, 0, 6, 8, 5, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 2},
-		{0, 6, 0, 0, 1, 9, 0, 3, 8},
-		{0, 0, 1, 0, 0, 0, 0, 4, 0},
-		{0, 5, 0, 4, 0, 3, 0, 0, 0},
-		{3, 0, 0, 8, 0, 0, 0, 0, 6},
-		{4, 2, 7, 5, 6, 0, 9, 0, 0},
-		{0, 0, 5, 0, 0, 2, 0, 8, 0},
-		{0, 8, 0, 0, 0, 7, 0, 0, 0},
+	easy := [9][9]int{
+		{0, 8, 0, 7, 0, 9, 0, 0, 2},
+		{0, 3, 4, 0, 1, 0, 0, 9, 0},
+		{0, 0, 0, 3, 0, 8, 0, 0, 0},
+		{0, 0, 6, 4, 3, 0, 8, 0, 1},
+		{0, 0, 1, 2, 7, 6, 0, 4, 0},
+		{0, 0, 3, 0, 0, 1, 2, 5, 6},
+		{0, 0, 0, 0, 9, 0, 0, 2, 7},
+		{3, 4, 0, 8, 6, 7, 9, 0, 0},
+		{0, 9, 0, 5, 0, 4, 0, 0, 3},
 	}
+
+	// medium := [9][9]int{
+	// 	{1, 3, 0, 6, 8, 5, 0, 0, 0},
+	// 	{0, 0, 0, 0, 0, 0, 0, 0, 2},
+	// 	{0, 6, 0, 0, 1, 9, 0, 3, 8},
+	// 	{0, 0, 1, 0, 0, 0, 0, 4, 0},
+	// 	{0, 5, 0, 4, 0, 3, 0, 0, 0},
+	// 	{3, 0, 0, 8, 0, 0, 0, 0, 6},
+	// 	{4, 2, 7, 5, 6, 0, 9, 0, 0},
+	// 	{0, 0, 5, 0, 0, 2, 0, 8, 0},
+	// 	{0, 8, 0, 0, 0, 7, 0, 0, 0},
+	// }
 
 	// hard := [9][9]int{
 	// 	{0, 8, 0, 7, 0, 9, 0, 0, 2},
@@ -121,7 +120,6 @@ func init() {
 	// 	{0, 9, 0, 5, 0, 4, 0, 0, 3},
 	// }
 
-	puzzle = medium
-	guesses = *solver.InitializeGuesses(&puzzle)
+	puzzle = *solver.NewSudokuPuzzle(&easy)
 
 }
